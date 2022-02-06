443306b0614dfcee3306494c37b89ec9143f0968
Zaneta Poreba <zanetaporeba@gmail.com>
2022-02-06 20:56:04 +0100

    Add CORS to settings

--- server/settings.py
+++ server/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
-MIDDLEWARE = [ 
-
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,4 +128,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     'https://localhost:3000'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost",
+    "http://localhost:80",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
